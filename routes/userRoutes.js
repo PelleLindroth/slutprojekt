b632297b646f418b5f3dec49fed4deffb7b250e3
renzosantamaria <renzo.santamaria@outlook.com>
2021-05-06 11:52:39 +0200

    Test route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,23 @@
 const express = require('express')
 const userRoutes = express.Router()
+const userController = require('../controllers/userController')
+const authToken = require('../middleware/authToken')
 const User = require('../models/userModel')
+const {Unauthorized} = require('../errors/index')
 
-// userRoutes.get('/users', async (req, res) => {
-//   const users = await User.findAll()
-//   res.json({ users })
-// })
+
+userRoutes.get('/users', authToken, async (req, res, next) => {
+    try{
+        console.log(req.user);
+        // throw new Unauthorized()
+        const users = await User.findAll()
+        res.json({ users })
+    }catch(error){
+        next(error)
+    }
+  
+})
+
+userRoutes.post('/authenticate', userController.authenticate)
 
 module.exports = userRoutes
