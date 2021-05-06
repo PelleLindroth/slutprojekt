565c1d2deb6534cbf628fcfb0ff8062938ecfc5d
PelleLindroth <pellelindroth@gmail.com>
2021-05-06 10:12:06 +0200

    Add cors

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require("express");
 require("dotenv").config();
 const fileUpload = require("express-fileupload");
+const cors = require('cors');
 const userRoutes = require("./routes/userRoutes");
 const taskRoutes = require("./routes/taskRoutes");
-
 const PORT = process.env.PORT || 5000;
-
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(fileUpload());
 
