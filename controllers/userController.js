68ccc9dd6e6b98efb51f49334ad5336e4a3e3189
Emma Dawson <emma.l.dawson@gmail.com>
2021-05-06 14:23:57 +0200

    Add POST createUser and GET me routes and controllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,15 +1,59 @@
-const User = require('../models/userModel')
+const { MissingCredentials } = require("../errors");
+const { Op } = require("sequelize");
+const User = require("../models/userModel");
+const Task = require("../models/taskModel");
 
 const authenticate = async (req, res, next) => {
-    try{
-        const user = await User.authenticate(req.body)
-        res.json(user)
-    }catch(error){
-        console.log(error.errorMessage)
-        next(error)
+  try {
+    const user = await User.authenticate(req.body);
+    res.json(user);
+  } catch (error) {
+    console.log(error.errorMessage);
+    next(error);
+  }
+};
+
+const createUser = async (req, res, next) => {
+  try {
+    const { email, password, name, role } = req.body;
+    if (!email || !password || !name) {
+      throw new MissingCredentials(["email", "password", "name"]);
     }
-}
+    const newUser = await User.create({ email, password, name, role });
+    res.json({
+      id: newUser.id,
+      name: newUser.name,
+      email: newUser.email,
+      role: newUser.role,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getOwnAccount = async (req, res, next) => {
+  try {
+    const user = await User.findOne({
+      where: { email: req.user.email },
+    });
+
+    if (req.user.role === "admin") {
+      res.json({ name: user.name, email: user.email, role: user.role });
+    } else {
+      const tasks = await Task.findAll({
+        where: {
+          [Op.or]: [{ workerId: req.user.id }, [{ clientId: req.user.id }]],
+        },
+      });
+      res.json({ name: user.name, email: user.email, role: user.role, tasks });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
 
 module.exports = {
-    authenticate
-}
+  authenticate,
+  createUser,
+  getOwnAccount,
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,23 +1,36 @@
-const express = require('express')
-const userRoutes = express.Router()
-const userController = require('../controllers/userController')
-const authToken = require('../middleware/authToken')
-const User = require('../models/userModel')
-const {Unauthorized} = require('../errors/index')
+const express = require("express");
+const userRoutes = express.Router();
+const userController = require("../controllers/userController");
+const authToken = require("../middleware/authToken");
+const authRoles = require("../middleware/authRole");
+const User = require("../models/userModel");
+const { Unauthorized } = require("../errors/index");
 
+// userRoutes.get(
+//   "/users",
+//   authToken,
+//   authRoles(["client", "admin"]),
+//   async (req, res, next) => {
+//     try {
+//       console.log(req.user);
+//       // throw new Unauthorized()
+//       const users = await User.findAll();
+//       res.json({ users });
+//     } catch (error) {
+//       next(error);
+//     }
+//   }
+// );
 
-userRoutes.get('/users', authToken, async (req, res, next) => {
-    try{
-        console.log(req.user);
-        // throw new Unauthorized()
-        const users = await User.findAll()
-        res.json({ users })
-    }catch(error){
-        next(error)
-    }
-  
-})
+userRoutes.post("/authenticate", userController.authenticate);
 
-userRoutes.post('/authenticate', userController.authenticate)
+userRoutes.post(
+  "/users",
+  authToken,
+  authRoles(["admin"]),
+  userController.createUser
+);
 
-module.exports = userRoutes
+userRoutes.get("/me", authToken, userController.getOwnAccount);
+
+module.exports = userRoutes;
