559643c7cfa41c026044330b4eac190e96301fa0
renzosantamaria <renzo.santamaria@outlook.com>
2021-05-06 11:51:24 +0200

    Start Auth and Error Middleware

--- app.js
+++ app.js
@@ -4,14 +4,21 @@ const fileUpload = require("express-fileupload");
 const cors = require('cors');
 const userRoutes = require("./routes/userRoutes");
 // const taskRoutes = require("./routes/taskRoutes");
+const errorHandler = require('./middleware/errorHandler')
+const Logger = require('./middleware/logger')
 const PORT = process.env.PORT || 5000;
 const app = express();
+
 
 app.use(cors());
 app.use(express.json());
 app.use(fileUpload());
 
+app.use(Logger)
+
 app.use("/api/v1", userRoutes);
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
---
+++ errors/index.js
@@ -0,0 +1,77 @@
+class CustomError extends Error{}
+
+class Forbidden extends CustomError {
+    constructor() {
+        super()
+        this.errorCode = 403
+        this.errorMessage = "Access denied!"
+    }
+}
+
+class ResourceNotFound extends CustomError{
+    constructor(payload){
+    super()
+    this.errorCode = 404
+    this.errorMessage = `${payload} not found.`
+    }
+}
+
+class InvalidCredentials extends CustomError{
+    constructor(){
+    super()
+    this.errorCode = 401
+    this.errorMessage = `Email and password don't match`
+    }
+}
+
+class Unauthorized extends CustomError{
+    constructor(){
+    super()
+    this.errorCode = 401
+    this.errorMessage = `Unauthorized.`
+    }
+}
+
+class TokenExpired extends CustomError{
+    constructor(){
+    super()
+    this.errorCode = 401
+    this.errorMessage = `Token has expired, please log in again.`
+    }
+}
+
+class InvalidBody extends CustomError{
+    constructor(...arr){
+    super()
+    this.errorCode = 400
+    this.errorMessage = `${arr.join(', ')} required.`
+    }
+}
+
+class UnsupportedFileType extends CustomError{
+    constructor(){
+    super()
+    this.errorCode = 415
+    this.errorMessage = `Unsupported file type.`
+    }
+}
+
+class Teapot extends CustomError{
+    constructor(){
+    super()
+    this.errorCode = 418
+    this.errorMessage = `I'm a Teapot, yes you are.`
+    }
+}
+
+module.exports = {
+    Teapot,
+    CustomError,
+    Forbidden,
+    ResourceNotFound,
+    InvalidCredentials,
+    Unauthorized,
+    InvalidBody,
+    UnsupportedFileType,
+    TokenExpired
+}
