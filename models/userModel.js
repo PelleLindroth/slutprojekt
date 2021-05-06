af6143ab3abb06a6441a9365b0b48b649f25b7ce
renzosantamaria <renzo.santamaria@outlook.com>
2021-05-06 11:52:22 +0200

    Add auithenticate and validateToken functions

--- models/userModel.js
+++ models/userModel.js
@@ -4,6 +4,7 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const Task = require("./taskModel");
 const Message = require("./messageModel");
+const {InvalidCredentials, Unauthorized, TokenExpired} = require('../errors/index')
 
 const User = db.define("User", {
   name: {
@@ -35,23 +36,49 @@ User.belongsToMany(Task, { through: Message });
 Task.belongsToMany(User, { through: Message });
 
 
-User.beforeCreate((user, options) => {
+User.beforeCreate((user) => {
   user.password = bcrypt.hashSync(user.password, 10)
 })
 
-// User.authenticate = async ({ email, password }) => {
-//   const user = await User.findOne({ where: { email } })
-//   if (!user) return ({ success: false, message: 'Invalid credentials. Please check your email' })
+User.authenticate = async ({ email, password }) => {
 
-//   const valid = bcrypt.compareSync(password, user.password)
+  const user = await User.findOne({ where: { email } })
+  if (!user) {
+    throw new InvalidCredentials()
+  }
 
-//   if (valid) {
-//     const payload = { email, id: user.id }
-//     const token = jwt.sign(payload, process.env.JWT_SECRET)
-//     return { success: true, user: { id: user.id, name: user.name, email: user.email, favoriteMix: user.favoriteMix }, token }
-//   } else {
-//     return ({ success: false, message: 'Access denied. Your email and password don\'t match' })
-//   }
-// }
+  const valid = bcrypt.compareSync(password, user.password)
+
+  if (!valid) {
+    throw new InvalidCredentials()
+  }
+  console.log(process.env.JWT_SECRET);
+  const payload = { email, id: user.id, role: user.role }
+  const token = jwt.sign(payload, process.env.JWT_SECRET)
+  console.log(payload);
+  console.log(token);
+  return { 
+    id: user.id,
+    name: user.name,
+    email: user.email,
+    role: user.role,
+    token 
+  }
+
+}
+
+User.validateToken = (token) => {
+  try {
+    return jwt.verify(token, process.env.JWT_SECRET);
+  } catch (error) {
+    if (error instanceof jwt.TokenExpiredError) {
+      throw new TokenExpired();
+    } else if (error instanceof jwt.JsonWebTokenError) {
+      throw new Unauthorized();
+    } else {
+      throw error;
+    }
+  }
+}
 
 module.exports = User;
