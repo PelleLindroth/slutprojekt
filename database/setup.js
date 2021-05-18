f4e71b614499ddb4f2fb34b77377306d6c835d55
PelleLindroth <pellelindroth@gmail.com>
2021-05-18 10:04:22 +0200

    Add Review and Report models to setup.js

--- database/setup.js
+++ database/setup.js
@@ -4,5 +4,7 @@ require('../models/userModel')
 require('../models/imageModel')
 require('../models/taskModel')
 require('../models/messageModel')
+require('../models/reviewModel')
+require('../models/errorReportModel')
 
 db.sync({ force: true })
